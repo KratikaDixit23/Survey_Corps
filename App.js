af18e0ced750a88f37a673873522ee1263df1072
Kratika Dixit <128547616+KratikaDixit23@users.noreply.github.com>
2024-07-21 09:26:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Feature from './components/Feature';
+
+
+function App() {
+  return (
+    <div className="App bg-black">
+      <Navbar/>
+      <Header/>
+      <Feature/>
+    </div>
+  );
+}
+
+export default App;
+
